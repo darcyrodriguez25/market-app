cef3290e3a2d48f21be7879d6c357f6e3e330c31
darcy r <d3116981927@gmail.com>
2025-10-02 11:18:15 -0500

    main.php

--- src/signin.php
+++ src/signin.php
@@ -3,9 +3,12 @@
     //step1. Get database connection
     require('../config/database.php');
    
-    $e_mail = $_POST ['email'];
-    $p_wd = $_POST ['password'];
-
+    $e_mail = trim($_POST ['email']);
+    $p_wd = trim ($_POST ['passwd']);
+   
+    //$enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+    $enc_pass = md5($p_wd);
+    
     //Step 3. Query to validate data
     $sql_check_user = "
 select
@@ -15,15 +18,16 @@ from
     users u
 where
     u.email ='$e_mail' and
-    u.password ='$p_wd'
+    u.password ='$enc_pass'
    limit 1
     
    ";
     //step 4. execute query 
    $res_check =pg_query($sql_check_user);
    if(pg_num_rows($res_check) > 0){
-         echo "User exists. Go to main page!!!";
-     } else {
+         //echo "User exists. Go to main page!!!";
+    header('refresh:0;url=main.php');
+        } else {
         echo "Verify data";
     }
         
--- src/signup.php
+++ src/signup.php
@@ -2,13 +2,14 @@
 
     //step1. Get database connection
     require('../config/database.php');
-    $f_name = $_POST ['fname'];
-    $l_name = $_POST ['lname'];
-    $m_number = $_POST ['mnumber'];
-    $id_number = $_POST ['idnumber'];
-    $e_mail = $_POST ['email'];
-    $p_wd = $_POST ['password'];
-    //$enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
+    $f_name = trim ($_POST ['fname']);
+    $l_name = trim ($_POST ['lname']);
+    $m_number = trim ($_POST ['mnumber']);
+    $id_number = trim ($_POST ['idnumber']);
+    $e_mail = trim ($_POST ['email']);
+    $p_wd = trim ($_POST ['password']);
+    
+   // $enc_pass = password_hash($p_wd, PASSWORD_DEFAULT);
     $enc_pass = md5($p_wd);
      
     $check_email = "
@@ -41,7 +42,7 @@
         '$m_number',
         '$id_number',
         '$e_mail',
-        '$p_wd'
+        '$enc_pass'
     )
     ";
 
